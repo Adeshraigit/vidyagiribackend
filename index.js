e2d41dd6b6cdf3984e42bba9ac80cfc1cef4871a
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2025-03-21 21:51:48 +0530

    fixed cors error

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("*", cors()); 
+// app.options("*", cors()); 
 app.use(bodyParser.json());
 app.use(clerkMiddleware());
 
